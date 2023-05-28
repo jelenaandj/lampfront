39c66c0d03b5a9323e45544b5e3230a3c8e2c348
jelenaandj <jelena.andjelkovic453@gmail.com>
2023-05-28 19:09:26 +0200

    fix

---
+++ docs/precache-manifest.3cd80ba08fd99d2b79e63ab18d5276f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988aa45e48e75f04e99e00c069bb0b34",
+    "url": "/lampfront/index.html"
+  },
+  {
+    "revision": "408d777cf50be1e74bee",
+    "url": "/lampfront/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b286f93e251947cb8a1c",
+    "url": "/lampfront/static/css/main.f08e6f88.chunk.css"
+  },
+  {
+    "revision": "408d777cf50be1e74bee",
+    "url": "/lampfront/static/js/2.5c101a7e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lampfront/static/js/2.5c101a7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b286f93e251947cb8a1c",
+    "url": "/lampfront/static/js/main.9499cac4.chunk.js"
+  },
+  {
+    "revision": "0eb5017b6e80f64bc683",
+    "url": "/lampfront/static/js/runtime-main.18feb02a.js"
+  }
+]);
---
+++ docs/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lampfront/precache-manifest.3cd80ba08fd99d2b79e63ab18d5276f3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lampfront/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
